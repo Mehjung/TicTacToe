a3630e89afa7811720d703f0cc02de8c9a6a6193
Mehjung <89900707+Mehjung@users.noreply.github.com>
2023-01-21 17:02:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -94,8 +94,9 @@ class Game extends React.Component {
 }
 
 // ========================================
+const root = ReactDOM.createRoot(document.getElementById("container"));
+root.render(/React.createElement(Game, null));
 
-ReactDOM.render( /*#__PURE__*/React.createElement(Game, null), document.getElementById("container"));
 function calculateWinner(squares) {
   const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
   for (let i = 0; i < lines.length; i++) {
